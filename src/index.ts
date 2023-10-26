22f24f318d2dd42c181049b516e2a511f016eb48
spetushkou <sergey.petushkou@gmail.com>
2023-10-26 20:31:59 +0200

    feat: :rocket: #NOREF-0

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+/**
+ * description goes here...
+ * @example const point = new Point(7, 9);
+ * point.getX();
+ * point.getY();
+ */
+export class Point {
+  /**
+   * description goes here...
+   */
+  constructor() {
+    // ...
+  }
+
+  /**
+   * description goes here...
+   * @throws {DivideByZero} Argument x must be non-zero.
+   * @deprecated since version 2.0
+   */
+  getX() {
+    // ...
+  }
+
+  /**
+   * description goes here...
+   */
+  getY() {
+    // ...
+  }
+}
